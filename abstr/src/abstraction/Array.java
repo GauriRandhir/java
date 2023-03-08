45af1a15485a863dbfdc90c54764d7bdbce58cb8
PVG <PVG@172.16.12.128>
2023-03-08 09:32:19 -0500

    commit

---
+++ abstr/src/abstraction/Array.java
@@ -0,0 +1,17 @@
+package abstraction;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int arr[]= {8,0,98,87,-9,56};
+	
+		/*
+		 * arr[0]=1; arr[1]=15; arr[2]=17; arr[3]=10; arr[4]=13; arr[5]=12;
+		 */
+		
+		for(int i:arr)
+
+         System.out.print(i);
+	}
+
+}
